733d7f40d12d9be8a869697e98e30226aa1519c4
Manzai-1 <0xdecentmaz@gmail.com>
2025-09-10 07:17:07 +0200

    Add initial datastructure for subs and user -> sub maintance, add note regarding of-chain indexing to readme

--- contracts/SmartSub.sol
+++ contracts/SmartSub.sol
@@ -2,5 +2,30 @@
 pragma solidity 0.8.28;
 
 contract SmartSub {
-    
+
+    enum subState { 
+        active, 
+        paused 
+    }
+
+    struct Subscription {
+        string title;
+        uint256 id;
+        uint256 durationDays;
+        uint256 priceWei;
+        subState state; 
+        address owner; 
+        bool exists;
+    }
+
+    struct userSubscription {
+        uint256 subId;
+        uint256 durationDays;
+    }
+
+    mapping(uint256 => Subscription) public subscriptions;
+    mapping(address => mapping(
+        uint256 => userSubscription
+    )) public userSubscriptions;
+
 }
